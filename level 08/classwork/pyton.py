1ca54862baa36d38dd780d414b36bdf23b76fdb8
dateba3 <datodatebashvili4@gmail.com>
2025-03-08 05:40:35 -0800

    second commit

---
+++ level 08/classwork/pyton.py
@@ -0,0 +1,19 @@
+#1) Name = "Nika"
+#naMe = "Luka"
+#NAME = "Data"
+#naME = "Vano"
+#name = "Demetre"
+#NaMe = "Jan"
+#name = "Giorgi"
+
+#რამდენი ცვლადი შეიქმნება ამ კოდის მიხედვით?(აღწერეთ რატომ)
+#შეიქქმნება 5 რადგანაც 5 არის ყველასგან განსხვავებული
+
+2#) შექმენით 5 ცვლადი, snake_case-ის გამოყენებით და დაბეჭდეთ```
+name_people=("dato")
+name_people=("nika")
+name_people=("gio")
+name_people=("deme")
+name_people=("maia")
+
+print(name_people)
---
+++ level 08/homework/pyton.py
@@ -0,0 +1,26 @@
+#3) კომენტარებით ახსენით თუ რა არის snake_case და რისთვის გამოიყენება ის. მოიყვანეთ მაგალითები
+#snake_case არის იგივე სპეისის გამოყენება ცვლადში მაგალითად ცვლადი new_world new_phone
+
+
+#4) რას გამოიტანს შემდეგი კოდი და რატომ:
+#number1 = 10
+#number2 = 20
+#sum_of_numbers = number1 + number2
+#print(sum_Of_numbers)
+#არაფერს რადგან ცვლად sum_Of_numbers დაწერილია არასწორად და გამოიტანს ERORS
+
+
+#5) დაწერეთ პროგრამა, რომელიც გამოითვლის 5 რიცხვის საშუალო არითმეტიკულს, მიღებული რიცხვი შეინახეთ ცვლადში, აიყვანეთ კუბში(მესამე ხარისხში) და დაბეჭდეთ.```
+number1=3
+number2=4
+number3=5
+number4=7
+number5=6
+
+aritmetikuli=number1+number2+number3+number4+number5
+print(aritmetikuli/5)
+
+jami=5
+
+
+print(jami*5*5)
