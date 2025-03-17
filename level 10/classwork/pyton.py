1e9da534f896f1f21ac7add2aea02bcc14b988b9
dateba3 <datodatebashvili4@gmail.com>
2025-03-17 08:51:50 -0700

    second commit

---
+++ level 10/classwork/pyton.py
@@ -0,0 +1,13 @@
+#1) მომხარებელს შემოატანინეთ სახელი და გვარი, შეინახეთ ისინი ცვლადებში და დაბეჭდეთ მისთვის მისალმება(მაგალითად: "Hello name surnmae")
+name=input("please enter you name: ")
+print(name)
+username=input("please enter you username: ")
+print(username)
+
+#2) მომხარებელს შემოატანინეთ 3 რიცხვი და დაბეჭდეთ მათი ნამრვალი
+
+number1=input("please enter you number: ")
+number2=input("please enter you number: ")
+number3=input("pleaase enter you numbr: ")
+
+print(int(number1)*int(number2)*int(number3))
---
+++ level 10/homework/pyton.py
@@ -0,0 +1,16 @@
+#2) კომენტარებით ახსენით რა არის input/output-ი და მოიყვანეთ მათი მაგალითები.
+#input არის რაგაცის შეყვანა ან ეკრანზე გაკეთება მაგალითდ:კლავიატურა არის input
+#outoput არის ის რაც მაგალითად რაც გვესმის ან ეკრანზე გამოტანა ან რამის გაგება 
+
+#3) მომხმარებელს შემოატანინეთ ორი სიტყვა, შეინახეთ ისინი ცვლადში, მოახდინეთ მათი კონკადინაცია და დაბეჭდეთ
+
+#4) მომხმარებელს შემოატანინეთ 5 რიცხვი და დაბეჭდეთ მათი საშუალო არითმეტიკული.
+number1=input("plase enter you social number: ")
+number2=input("please enter you card number: ")
+number3=input("please enter you date number: ")
+number4=input("please enter you number to get it: ")
+number5=input("please enter you code that will be sent in you phone: ")
+  
+print(int(number1)+int(number2)+int(number3)+int(number4)+int(number5)/5)
+
+#5) მომხმარებელს შემოატანინეთ სახელი, გვარი, ასაკი, სიმაღლე, წონა და მათი გამოყენებით დაბეჭდეთ წინადადება.
