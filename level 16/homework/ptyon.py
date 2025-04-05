eb51e62feeda336545b473d863f6225065f76329
dateba3 <datodatebashvili4@gmail.com>
2025-04-05 08:00:36 -0700

    second commit

---
+++ level 16/homework/ptyon.py
@@ -0,0 +1,18 @@
+#2) კომენტარებით ახსენით რა არის sequence და selection. მოიყვანეთ მათი მაგალითები
+
+#3) კომენტარებით ახსენით რა არის Control Flow(ივარჯიშეთ if/else-ბზე და გააკეთეთ მინიმუმ 3 მაგალითი)
+
+#4) შექმენით ცვლადი, სადაც შეინახავთ თქვენს სახელს. მომხმარებელს შემოატანინეთ პაროლი და თუ ის უდრის "1234"-ს დაბეჭდეთ "Password is correct", სხვა შემთხვევაში დაბეჭდეთ "Password is incorrect"
+name="dato"
+pasword=1234(input("please enter pasword: "))
+if pasword==1234:
+    print("you pasword is corect")
+else:
+    print("you pasword is incorect")
+
+#5) მომხმარებელს შემოატანინეთ რიცხვი, თუ ის ლუწია დაბეჭდეთ "Number is Even", სხვა შემთხვევაში დაბეჭდეთ "Number is Odd"```
+#num=int(input("please enter you number: "))
+#if num%2==0:
+    print("even")
+#else:
+ #   print("odd")
