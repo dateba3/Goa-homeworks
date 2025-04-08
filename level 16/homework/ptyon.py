1c83b73d895ee2c35225be2bf489f3261e89131a
dateba3 <datodatebashvili4@gmail.com>
2025-04-08 07:19:27 -0700

    second commit

--- level 16/homework/ptyon.py
+++ level 16/homework/ptyon.py
@@ -1,6 +1,34 @@
 #2) კომენტარებით ახსენით რა არის sequence და selection. მოიყვანეთ მათი მაგალითები
 
+#sequence ნიშნავს ინსტრუქციების შესრულებას თანმიდევრობით მაგ: ჯერ აიღე წყალი და მერე პური
+
+#selection ნიშნავს რორა რაღაცის შემთვევაში რაღაც სრულდება მაგ: if გარეთ გშივა ჟამე else თუ არ გშივა არ ჭამო
+
 #3) კომენტარებით ახსენით რა არის Control Flow(ივარჯიშეთ if/else-ბზე და გააკეთეთ მინიმუმ 3 მაგალითი)
+
+#controlflow ნიშნავს როგორ სრულდბეა კოდი რა წესით 
+name="dato"
+pasword=1234(input("please enter pasword: "))
+if pasword==1234:
+    print("you pasword is corect")
+else:
+    print("you pasword is incorect")
+
+
+
+num=int(input("please enter you number: "))
+if num%2==0:
+    print("even")
+else:
+    print("odd")
+
+name1="dato"
+name=str(input("plase enter you name"))
+if name==name1:
+    print("good")
+else:
+    print("bad")
+
 
 #4) შექმენით ცვლადი, სადაც შეინახავთ თქვენს სახელს. მომხმარებელს შემოატანინეთ პაროლი და თუ ის უდრის "1234"-ს დაბეჭდეთ "Password is correct", სხვა შემთხვევაში დაბეჭდეთ "Password is incorrect"
 name="dato"
@@ -11,8 +39,8 @@ else:
     print("you pasword is incorect")
 
 #5) მომხმარებელს შემოატანინეთ რიცხვი, თუ ის ლუწია დაბეჭდეთ "Number is Even", სხვა შემთხვევაში დაბეჭდეთ "Number is Odd"```
-#num=int(input("please enter you number: "))
-#if num%2==0:
+num=int(input("please enter you number: "))
+if num%2==0:
     print("even")
-#else:
- #   print("odd")
+else:
+    print("odd")
