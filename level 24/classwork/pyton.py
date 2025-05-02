387492ed334ba9565301bff81a2199149b354349
dateba3 <datodatebashvili4@gmail.com>
2025-05-02 12:13:54 -0700

    second commit

---
+++ level 24/classwork/pyton.py
@@ -0,0 +1,24 @@
+#1) დაბეჭდეთ 0-დან 10-მდე ყველა რიცხვი, შემდეგ კი 0-დან 10-ის ჩათვლით
+number=0
+while number < 11:
+    print(number)
+    number=number+1
+    
+number=0
+while number < 10:
+    print(number)
+    number=number+1
+#2) დაბეჭდეთ 20-დან 100-ის ჩათვლით ყველა რიცხვი
+number=0
+while number <101:
+    print(number)
+    number=number+1
+
+#3) დაბეჭდეთ 0-დან 20-ის ჩათვლით ყველა ლუწი რიცხვი(ყოველი მეორე რიცხვი)
+number=0
+while number <21:
+    print(number)
+    number=number+2
+#4) დაბეჭდეთ 30-დან 50-მდე ყოველი მეოთხე რიცხვი
+
+#გააკეთეთ ეს დავალებები while ციკლით
